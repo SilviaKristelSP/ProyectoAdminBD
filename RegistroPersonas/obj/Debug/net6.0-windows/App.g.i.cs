7f5ba870fe4cd61fadb2d856c4a643ddfa88c8f7
Axelpm <64749855+Axelpm@users.noreply.github.com>
2022-06-14 01:22:23 -0500

    Daos

--- RegistroPersonas/obj/Debug/net6.0-windows/App.g.i.cs
+++ RegistroPersonas/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace RegistroPersonas {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace RegistroPersonas {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
         public static void Main() {
             RegistroPersonas.App app = new RegistroPersonas.App();
             app.InitializeComponent();
