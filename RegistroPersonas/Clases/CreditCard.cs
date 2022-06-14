203413a7fe318a49f4a9de6ec8db194aa21fab5a
Kristel Sanchez <huachupango-@hotmail.com>
2022-06-14 02:01:30 -0500

    Añade las nuevas clases y DAO

---
+++ RegistroPersonas/Clases/CreditCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroPersonas.Clases
+{
+    internal class CreditCard
+    {
+        public int Id { get; set; }
+
+        public string CardType { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public int ExpMonth { get; set; }
+
+        public int ExpYear { get; set; }
+
+    }
+}
